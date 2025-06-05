3beb69fc5d9d6e92fdb2961d783c7a650beb84f9
Dhaval <dp9718776@gmail.com>
2025-06-05 16:42:24 +0530

    Update

--- src/ImGuiCustomTheme.h
+++ src/ImGuiCustomTheme.h
@@ -1,20 +1,17 @@
 #pragma once
 
-#include <rlImGui.h>
-#include <imgui.h>
-
-inline void ImCustomTheme()
+inline void ImCustomTheme(const std::string& path = "assets/fonts/Roboto-Regular.ttf")
 {
-    // Load a custom font
-    ImGuiIO &io = ImGui::GetIO();
-    io.Fonts->Clear();
-    io.Fonts->AddFontFromFileTTF("assets/fonts/Roboto-Regular.ttf", 20.0f);
-    rlImGuiReloadFonts();
+	// Load a custom font
+	ImGuiIO& io = ImGui::GetIO();
+	io.Fonts->Clear();
+	io.Fonts->AddFontFromFileTTF(path.c_str(), 20.0f);
+	rlImGuiReloadFonts();
 
-    // Apply a simple dark-gray theme to frame backgrounds and rounding
-    ImGuiStyle &style = ImGui::GetStyle();
-    style.Colors[ImGuiCol_FrameBg]        = ImColor(0.22f, 0.22f, 0.22f, 1.0f);
-    style.Colors[ImGuiCol_FrameBgHovered] = ImColor(0.20f, 0.20f, 0.20f, 1.0f);
-    style.Colors[ImGuiCol_FrameBgActive]  = ImColor(0.30f, 0.30f, 0.30f, 1.0f);
-    style.WindowRounding                  = 5.0f;
+	// Apply a simple dark-gray theme to frame backgrounds and rounding
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.Colors[ImGuiCol_FrameBg] = ImColor(0.22f, 0.22f, 0.22f, 1.0f);
+	style.Colors[ImGuiCol_FrameBgHovered] = ImColor(0.20f, 0.20f, 0.20f, 1.0f);
+	style.Colors[ImGuiCol_FrameBgActive] = ImColor(0.30f, 0.30f, 0.30f, 1.0f);
+	style.WindowRounding = 5.0f;
 }
